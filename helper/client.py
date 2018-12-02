fb2cb7c12d71c39fe2194b1781f62405ef0bae41
Giorgi Kharshiladze <giorgi.kharshila@gmail.com>
2018-12-02 16:46:44 -0500

    helper client server functions working locally

--- helper/client.py
+++ helper/client.py
@@ -1,19 +1,19 @@
 import socket
  
 host = '127.0.0.1'
-port = 50008
+port = 50009
  
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
 print("Connected to "+(host)+" on port "+str(port))
 initialMessage = input("Send: ")
-s.sendall(initialMessage)
+s.sendall(initialMessage.encode('utf-8'))
  
 while True:
- data = s.recv(1024)
- print("Recieved: "+(data))
- response = input("Reply: ")
- if response == "exit":
-     break
- s.sendall(response)
+	data = s.recv(1024)
+	print("Recieved: "+(data.decode('utf-8')))
+	response = input("Reply: ")
+	if response == "exit":
+	 break
+	s.sendall(response.encode('utf-8'))
 s.close()
--- helper/server.py
+++ helper/server.py
@@ -1,7 +1,7 @@
 import socket
  
 host = '127.0.0.1'
-port = 50008
+port = 50009
  
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host, port))
@@ -11,9 +11,9 @@ print ("Connection from", addr)
 while True:
     data = conn.recv(1024)
     if not data: break
-    print("Recieved: "+(data))
+    print("Recieved: "+(data.decode('utf-8')))
     response = input("Reply: ")
     if response == "exit":
         break
-    conn.sendall(response)
+    conn.sendall(response.encode('utf-8'))
 conn.close()
