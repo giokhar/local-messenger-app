18163674c2328bce5be0a7cdff6eb86287e5c250
Giorgi Kharshiladze <giorgi.kharshila@gmail.com>
2018-12-02 21:32:46 -0500

    data instead of msg

--- static/js/custom.js
+++ static/js/custom.js
@@ -18,17 +18,17 @@ socket.on( 'connect', function() {
   } )
 } );
 
-socket.on( 'my response', function( msg ) {
-  console.log( msg )
+socket.on( 'my response', function( data ) {
+  console.log( data )
   let time = new Date().getHours() + ":" + ('0'+new Date().getMinutes()).slice(-2);
-  if (msg.my_message !== undefined) {
-    $( 'div.messages-body' ).append( '<div class="msg"><div class="message-reply-body you_"><div class="message-text">'+msg.my_message+'</div><div class="message-time">'+time+'</div></div></div>' )
+  if (data.my_message !== undefined) {
+    $( 'div.messages-body' ).append( '<div class="data"><div class="message-reply-body you_"><div class="message-text">'+data.my_message+'</div><div class="message-time">'+time+'</div></div></div>' )
   }
-  if (msg.friend_message !== undefined){
-    $( 'div.messages-body' ).append( '<div class="msg"><div class="message-reply-body friend"><div class="message-text">'+msg.friend_message+'</div><div class="message-time">'+time+'</div></div></div>' )
+  if (data.friend_message !== undefined){
+    $( 'div.messages-body' ).append( '<div class="data"><div class="message-reply-body friend"><div class="message-text">'+data.friend_message+'</div><div class="message-time">'+time+'</div></div></div>' )
   }
-  if (msg.new_user !== undefined){
-    $( 'div.panel-col' ).append( '<div class="recent-user" id="'msg.new_user'"><div class="recent-avatar"><img src="static/img/2.jpg" class="a0uk"></div><div class="chat-name-recent-message"><div class="recent-user-name">'+msg.new_user+'</div><div class="recent-user-message">No messages yet...</div></div><div class="message-count"></div></div>' )
+  if (data.new_user !== undefined){
+    $( 'div.panel-col' ).append( '<div class="recent-user" id="'data.new_user'"><div class="recent-avatar"><img src="static/img/2.jpg" class="a0uk"></div><div class="chat-name-recent-message"><div class="recent-user-name">'+data.new_user+'</div><div class="recent-user-message">No messages yet...</div></div><div class="message-count"></div></div>' )
   }
   // Always scroll down when message sent
   $('.messages--wrap').scrollTop(Number.MAX_SAFE_INTEGER)
