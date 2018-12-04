e059ccea491f33dc56547a6f14ebccdc8b18ff7b
Davit Kvarskhava <dkvart17@earlham.edu>
2018-12-03 22:23:45 -0500

    sender and receiver implemented. v1

--- helper/send_message.py
+++ helper/send_message.py
@@ -1,22 +1,21 @@
 import socket
  
-host = socket.gethostname()
+# host = socket.gethostname()
 port = 50010
  
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host, port))
-print("Connected to "+(host)+" on port "+str(port))
-initialMessage = input("Send: ")
-s.sendall(initialMessage.encode('utf-8'))
- 
-while True:
-	data = s.recv(1024)
-	print("Recieved: "+(data.decode('utf-8')))
-	response = input("Reply: ")
-	if response == "exit":
-	 break
-	s.sendall(response.encode('utf-8'))
-s.close()
+def send_message_to(host):
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((host, port))
+	print("Connected to "+(host)+" on port "+str(port))
+
+	while True:
+		initialMessage = input("Send: ")
+		s.sendall(initialMessage.encode('utf-8'))
+		if initalMessage == "exit":
+			break
+		#need to break when EXIT is entered
+
+	s.close()
 
 # def get_free_tcp_port():
 #     tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -24,5 +23,3 @@ s.close()
 #     addr, port = tcp.getsockname()
 #     tcp.close()
 #     return port
-
-# def send_message(ip_to_send, port = 50009,  )
--- helper/server.py
+++ helper/server.py
@@ -2,30 +2,46 @@ import socket
 import threading
 
 def handle_new_client(conn, addr):
-	print("by")
 	while True:
-	    data = conn.recv(1024)
-	    if not data: break
-	    print("hey")
-	    print("Recieved: "+(data.decode('utf-8')))
-	    response = input("Reply: ")
+		data = conn.recv(1024)
+		if not data: break
+		dec_data = data.decode('utf-8')
+		print("Recieved: "+ dec_data)
 
-	    if response == "exit":
-	        break
-
-	    conn.sendall(response.encode('utf-8'))
+		if dec_data == "exit":
+			break
+			
 	conn.close()
 
-host = socket.gethostname()
-port = 50010
- 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((host, port))
-s.listen(5)
+def receive_new_message():
+	host = socket.gethostname()
+	port = 50010
+	 
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.bind((host, port))
+	s.listen(6)
 
-while True:
-	conn, addr = s.accept()
-	print ("Connection from", addr)
-	new_client_thread = threading.Thread(target = handle_new_client, args = (conn,addr,))
-	new_client_thread.start()
-s.close()
+	while True:
+		conn, addr = s.accept()
+		print ("Connection from", addr)
+		#A thread shuts down itself after handling new client.
+		new_client_thread = threading.Thread(target = handle_new_client, args = (conn,addr,))
+		new_client_thread.start()
+	s.close()
+
+receive_new_message()
+# host = socket.gethostname()
+# port = 50010
+ 
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.bind((host, port))
+# s.listen(5)
+
+# while True:
+# 	conn, addr = s.accept()
+# 	print ("Connection from", addr)
+# 	#A thread shuts down itself after handling new client.
+# 	new_client_thread = threading.Thread(target = handle_new_client, args = (conn,addr,))
+# 	threads.append(new_client_thread)
+# 	new_client_thread.start()
+# s.close()
